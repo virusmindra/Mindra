83668baf7ba51021bd50cb49bde844c1f53a0b99
virusmindra <tsykhotskyi@icloud.com>
2025-06-21 01:15:37 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-# Установка FFmpeg
+# Устанавливаем ffmpeg локально в директорию проекта
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
-mv ffmpeg-*-amd64-static/ffmpeg /usr/local/bin/
-chmod +x /usr/local/bin/ffmpeg
+mkdir -p ./bin
+mv ffmpeg-*-amd64-static/ffmpeg ./bin/
+chmod +x ./bin/ffmpeg
+
+# Добавляем ffmpeg в PATH
+export PATH=$PATH:$(pwd)/bin
 
 # Установка Python-зависимостей
+pip install --upgrade pip
+pip install --no-cache-dir --upgrade setuptools wheel
 pip install -r requirements.txt
