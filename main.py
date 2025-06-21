8949a7c116dc0568361bf9ff51b34c851ac3a83f
virusmindra <tsykhotskyi@icloud.com>
2025-06-22 01:13:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,6 +97,6 @@ if __name__ == '__main__':
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     application.add_handler(MessageHandler(filters.VOICE, handle_voice))  # если нужно
-
+    print("🤖 Mindra запущен!")
     application.run_polling()
 
