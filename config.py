af16b3175f44ad56305210c54b90a9e331d27cfb
virusmindra <tsykhotskyi@icloud.com>
2025-06-22 21:53:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+import os
+from openai import OpenAI
+
+TELEGRAM_BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
