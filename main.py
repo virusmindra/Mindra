418bf64b23ac5163e69b0b9729ab367eb4bf9ea7
virusmindra <tsykhotskyi@icloud.com>
2025-06-22 22:18:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # main.py
 
-import os
 from telegram.ext import ApplicationBuilder
 from handlers import handlers
+from config import TELEGRAM_BOT_TOKEN
 
-# Получаем токен бота из переменных окружения
-TELEGRAM_BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# Запуск бота
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    # Регистрируем все обработчики
     for handler in handlers:
         app.add_handler(handler)
 
