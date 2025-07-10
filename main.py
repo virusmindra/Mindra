49f31f6da42bcbbd24de600323a0df9c7f58f81e
virusmindra <tsykhotskyi@icloud.com>
2025-07-10 19:06:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -125,4 +125,7 @@ async def main():
     await app.run_polling()
     
 if __name__ == "__main__":
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    asyncio.get_event_loop().run_until_complete(main())
