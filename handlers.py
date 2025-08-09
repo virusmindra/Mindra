76cfabd52b1f8e6b45d2f74d47505eb3fe062197
virusmindra <tsykhotskyi@icloud.com>
2025-08-08 21:49:40 -0400

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -22,7 +22,7 @@ from history import load_history, save_history, trim_history
 from goals import  is_goal_like, goal_keywords_by_lang, REACTIONS_GOAL_DONE, DELETE_MESSAGES
 from pathlib import Path
 from apscheduler.schedulers.background import BackgroundScheduler
-from storage import delete_goal, load_goals, save_goals, add_goal, get_goals, get_goals_for_user, mark_goal_done
+from storage import delete_goal, load_goals, save_goals, add_goal, get_goals, get_goals_for_user, mark_goal_done, load_habits, save_habits, add_habit, get_habits, mark_habit_done, delete_habit
 from random import randint, choice
 from stats import load_stats, save_stats, get_premium_until, set_premium_until, is_premium, got_trial, set_trial, add_referral, add_points, get_user_stats, get_user_title, load_json_file, get_stats, OWNER_ID, ADMIN_USER_IDS 
 from telegram.error import BadRequest
