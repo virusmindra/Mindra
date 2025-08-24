742ea7c43c99975acb0bcdadc6ccd9831c1e3879
virusmindra <tsykhotskyi@icloud.com>
2025-08-24 15:24:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import (
     filters
 )
 from telegram.request import HTTPXRequest
-from stats import ensure_premium_db, migrate_premium_from_stats, load_stats
+from stats import ensure_premium_db, migrate_premium_from_stats, load_stats, ensure_remind_db
 from handlers import (
     start,
     language_callback,
