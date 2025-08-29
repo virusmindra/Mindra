1092229a2844f79eb7000e6c419cd576f581ff0b
virusmindra <tsykhotskyi@icloud.com>
2025-08-28 20:04:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import (
     filters
 )
 from telegram.request import HTTPXRequest
-from stats import ensure_premium_db, migrate_premium_from_stats, load_stats, ensure_remind_db
+from stats import ensure_premium_db, migrate_premium_from_stats, load_stats, ensure_remind_db, ensure_premium_challenges, PREMIUM_DB_PATH
 from handlers import (
     start,
     language_callback,
